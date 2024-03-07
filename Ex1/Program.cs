fe8f6f71b4d8161ff42293f312ca4f594ccbc940
Vitor França <francavit.r@gmail.com>
2024-03-07 19:12:21 -0300

    exercícios resolvidos

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -64,15 +64,45 @@ namespace exercicio1
             //comparar o tempo de execução de ambas as funções nos casos sugeridos
 
             var test1 = FiltraTerminadasEmALINQ(new List<string> {"Pera", "Maçã", "Banana", "Uva", "Abacate"}); // new List<string> {"Pera", "Banana", "Uva"}
+            foreach (var str in test1)
+            {
+                Console.Write($"{str} |");
+            }
+            Console.WriteLine();
             var test2 = FiltraTerminadasEmA(new List<string> { "Pera", "Maçã", "Banana", "Uva", "Abacate" }); // new List<string> {"Pera", "Banana", "Uva"}
+            foreach (var str in test2)
+            {
+                Console.Write($"{str} |");
+            }
+            Console.WriteLine();
             Console.WriteLine();
 
             var test3 = FiltraTerminadasEmALINQ(new List<string> {"BANANA", "AZEITE", "Sacola", "MERCADO"}); // new List<string> {"BANANA", "Sacola"}
+            foreach (var str in test3)
+            {
+                Console.Write($"{str} |");
+            }
+            Console.WriteLine();
             var test4 = FiltraTerminadasEmA(new List<string> { "BANANA", "AZEITE", "Sacola", "MERCADO" }); // new List<string> {"BANANA", "Sacola"}
+            foreach (var str in test4)
+            {
+                Console.Write($"{str} |");
+            }
+            Console.WriteLine();
             Console.WriteLine();
 
             var test5 = FiltraTerminadasEmALINQ(new List<string> {}); // new List<string> {}
+            if (test5.Count() == 0)
+            {
+                Console.WriteLine("{}");
+            }
             var test6 = FiltraTerminadasEmA(new List<string> { }); // new List<string> {}
+            if (test5.Count() == 0)
+            {
+                Console.WriteLine("{}");
+            }
+
+            Console.WriteLine();
             Console.WriteLine(@"Concluímos assim que, ao menos para um pequeno conjunto de
 itens, o método sem a utilização de LINQ é mais performático");
 
